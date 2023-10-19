a306ce92937664c441204c8e4205721ac7ddf9d2
Mohammad Aadil Shabier <aadilshabier1@gmail.com>
2023-10-19 11:54:17 +0530

    Fix bug in reading FAT
    Previously the sectors_per_fat wasn't considered whiel reading the FAT
    table. Now we read the redundant FATS and check for irregularities.
    
    - Change BootSector.root_dirs to max_root_dirs

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ struct BootSector {
 	uint8_t sectors_per_cluster;
 	uint16_t reserved_sectors;
 	uint8_t fats;
-	uint16_t root_dirs;
+	uint16_t max_root_dirs;
 	uint16_t sector_count;
     char ignore2[1];
 	uint16_t sectors_per_fat;
@@ -42,6 +42,8 @@ struct BootSector {
 class Floppy {
 public:
     bool read(ifstream& img) {
+		/* READ BOOT SECTOR */
+
 		img.read(reinterpret_cast<char*>(&boot), sizeof(boot));
 		if (unsigned sig = boot.boot_signature; sig != 0x28 and sig != 0x29) {
 			cerr << "ERROR: Signature is not valid: " << sig << endl;
@@ -51,31 +53,30 @@ public:
 		// skip reserved sectors 
 		img.seekg((boot.reserved_sectors-1)*SECTOR_SIZE, std::ios_base::cur);
 
-		// read FAT data
-		fat_data.resize(SECTOR_SIZE*boot.fats*sizeof(uint8_t)/sizeof(fat_data[0]));
-		img.read(reinterpret_cast<char*>(fat_data.data()), fat_data.size()*sizeof(fat_data[0]));
-		int active_cluster = 2;
-		int fat_offset = active_cluster + active_cluster/2;
-		auto fat_sector = 0 + (fat_offset / SECTOR_SIZE);
-		auto ent_offset = fat_offset % SECTOR_SIZE;
 
-		auto table_value = *(unsigned short*)&fat_data[ent_offset];
+		/* READ FAT */
 
-		table_value = (active_cluster & 1) ? table_value >> 4 : table_value & 0xfff;
-		// cout << (table_value == 0xFF8) << endl;
-		cout << hex << table_value << endl;
-		cout << dec;
+	    auto fat_table_size = boot.sectors_per_fat*SECTOR_SIZE;
+
+		fat_data.resize(fat_table_size);
+		img.read(reinterpret_cast<char*>(fat_data.data()), fat_data.size());
+
+		// check if redundant FAT copies match the first copy
+		{
+			vector<uint8_t> fat_data_copy(fat_data.size());
+			for (int i=0; i<boot.fats-1; i++) {
+				img.read(reinterpret_cast<char*>(fat_data_copy.data()), fat_data.size());
+				if (fat_data != fat_data_copy) {
+					cerr << "FAT copy " << i+1 << " does not match the original." << endl;
+					return false;
+				}
+			}
+		}
+
+
+		/* READ ROOT DIRECTORIES */
+
 		
-		// abc, DEF -> BC, AF, DE
- // for (int i=0; i<fat_data.size(); i++) {
-			// if (i % 2 == 0) {
-			// } else {
-			// }
-		// }
-
-		char lmao[33];
-		img.read(lmao, 33);
-		cout << lmao << endl;
 		
 		return true;
 	}
